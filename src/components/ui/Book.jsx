14837164bedca57137f32c3fbba3d615ec543c11
Noah <noahjames1035@gmail.com>
2025-06-28 18:23:00 -0400

    loading state debugging

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
+// debugging
 
 const Book = ({ book }) => {
   const [img, setImg] = useState();
